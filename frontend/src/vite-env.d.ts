528388b280c90d064ea8bb0b8b0b86ce7f58eb79
zdever <zdever1@gmail.com>
2026-05-11 16:06:25 +0200

    Delete frontend/src directory

--- frontend/src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-// frontend/src/vite-env.d.ts
-/// <reference types="vite/client" />
-
-// Zajistí že TypeScript rozumí CSS importům
-declare module '*.css' {
-  const content: string;
-  export default content;
-}
